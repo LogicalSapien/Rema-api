d9a6b6f6c74a47bd6245185b0f2509d8479aa4a9
Elmo Yeldo <elmoyeldo@gmail.com>
2021-07-09 12:03:51 +0100

    updating travis

--- src/test/java/com/logicalsapien/rema/RemaApplicationTests.java
+++ src/test/java/com/logicalsapien/rema/RemaApplicationTests.java
@@ -1,16 +1,16 @@
 package com.logicalsapien.rema;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class RemaApplicationTests {
 
-	@Test
-	void contextLoads() {
-		assertThat(true).isTrue();
-	}
+  @Test
+  void contextLoads() {
+    assertThat(true).isTrue();
+  }
 
 }
