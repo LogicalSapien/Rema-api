05fce189c8cda3a976f4a0bc3605fdeccbb930e4
Elmo Yeldo <elmoyeldo@gmail.com>
2021-07-12 13:13:48 +0100

    checkstyle fix

--- src/main/java/com/logicalsapien/rema/RemaApplication.java
+++ src/main/java/com/logicalsapien/rema/RemaApplication.java
@@ -3,9 +3,17 @@ package com.logicalsapien.rema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring boot application to load context & start application.
+ */
 @SpringBootApplication
-public class RemaApplication {//NOPMD - Main Class
+public class RemaApplication { //NOPMD - Main Class
 
+  /**
+   * Main method.
+   *
+   * @param args Program start arguments
+   */
   public static void main(final String[] args) {
     SpringApplication.run(RemaApplication.class, args);
   }
--- src/main/java/com/logicalsapien/rema/controller/HealthController.java
+++ src/main/java/com/logicalsapien/rema/controller/HealthController.java
@@ -6,9 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Health Check Controller.
+ * Health check apis return okay responses to periodic polling
+ */
 @RestController
 public class HealthController {
 
+  /**
+   * Returns okay response for health check polls.
+   *
+   * @return Okay Response
+   */
   @GetMapping("api/health")
   public ResponseEntity<Map<String, Object>> getHealth() {
     final Map<String, Object> healthResponse = new HashMap<>();
